5069301f649a47d24a090ee01390b3d0998cea3f
rafaviles <rafael@BLMOT-MACBOOK-01.local>
2014-11-24 02:55:20 +0100

    Update BasicJoin

--- java/src/dwdm/intersection/BasicJoin.java
+++ java/src/dwdm/intersection/BasicJoin.java
@@ -1,11 +1,20 @@
-package dwdm.intersection;
-
+//package dwdm.intersection
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Scanner;
+import java.util.Set;
+
 
 /**
  * If we think of a Hash Table as an array then a hash function is used to
@@ -20,65 +29,83 @@ import java.util.Scanner;
  */
 public class BasicJoin {
 	
-	private static final String DEFAULT_FILE1 = 
-			"/Users/rafael/Documents/intersection/intersection/resources/test1.txt";
-	private static final String DEFAULT_FILE2 =
-			"/Users/rafael/Documents/intersection/intersection/resources/test2.txt";
-			
+	private static final String fileEntry = "/Users/rafael/Downloads/f1/file1.txt";
+	private static final String fileEntry2 = "/Users/rafael/Downloads/f2/fila4.txt";
+	static List<Long> arr1 = new ArrayList<Long>();	
+	static List<Long> arr2 = new ArrayList<Long>();	
 
 	static String[] theArray;
+	int a;
 
-	static List<Long> ar1 = new ArrayList<Long>();
-	static List<Long> ar2 = new ArrayList<Long>();
+	//static HashSet<String> ar1 = new HashSet<String>();
+	//static HashSet<String> ar2 = new HashSet<String>();
+	
+	private final static File folder1 = new File("/Users/rafael/Downloads/f1/file4.txt");
+	private final static File folder2 = new File("/Users/rafael/Downloads/f2/fila4.txt");
+	  //File[] directoryListing = folder1.listFiles();
+	//public void listFilesForFolder(final File folder) {
+	//listFilesForFolder(folder);
 
-	public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String[] args) throws IOException {
 
-		File file1;
-		if(args.length < 1){
-			file1 = new File(DEFAULT_FILE1);
-		} else {
-			file1 = new File(args[0]);
-		}
-		Scanner scan1 = new Scanner(file1);
-
-		while (scan1.hasNextLine()) {
-			String lino = (scan1.nextLine().substring(1));
-			Long line = Long.parseLong(lino);
-			ar1.add(line);
-		}
-
-		File file2;
-		if(args.length < 2){
-			file2 = new File(DEFAULT_FILE2);
-		} else {
-			file2 = new File(args[1]);
-		}
-		Scanner scan2 = new Scanner(file2);
-
-		while (scan2.hasNextLine()) {
-			String lino = (scan2.nextLine().substring(1));
-			Long lina = Long.parseLong(lino);
-			ar2.add(lina);
-		}
-
-		Collections.sort(ar1);
-		Collections.sort(ar2);
-
-		System.out.println(ar1);
-		System.out.println(ar2);
-		scan1.close();
-		scan2.close();
-
-		List<Long> list = new ArrayList<Long>();
-
-		for (Long t : ar1) {
-			if (ar2.contains(t)) {
-				list.add(t);
+		//Set<Long> list = new HashSet<Long>();
+		BufferedWriter writer = new BufferedWriter(new FileWriter(new File("/Users/rafael/Downloads/outal.txt")));
+		//HashSet<String> ar1 = new HashSet<String>();
+		//HashSet<String> ar2 = new HashSet<String>();
+		
+		//for (File fileEntry : folder1.listFiles()) {
+			//HashSet<String> ar1 = new HashSet<String>();
+			BufferedReader reader = new BufferedReader(new FileReader(fileEntry));
+			while (reader.readLine() != null) {
+				String lino = (reader.readLine().substring(1));
+				Long line = Long.parseLong(lino);
+				arr1.add(line);
+				//Collections.sort(ar1);
 			}
-		}
+			//for (final File fileEntry2 : folder2.listFiles()) {
+				//HashSet<String> ar2 = new HashSet<String>();
+				BufferedReader reader2 = new BufferedReader(new FileReader(fileEntry2));
+				while (reader2.readLine() != null) {
+					String lino = (reader2.readLine().substring(1));
+					Long line = Long.parseLong(lino);
+					//System.out.print(lino + "\n");
+					arr1.add(line);
+					//Collections.sort(ar2);
+				}
+				
+				//Collections.sort(arr1);
+				System.out.print(arr1.size());
+				
+			    //int[] input = new int[]{1, 1, 3, 7, 7, 8, 9, 9, 9, 10};
+			    Long current = arr1.get(0);
+			    boolean found = false;
 
-		System.out.println(list);
-
+			    for (int i = 0; i < arr1.size(); i++) {
+			        if (current == arr1.get(i) && !found) {
+			            found = true;
+			            writer.write(Long.toString(current) + "\n");
+			        } else if (current != arr1.get(i)) {
+			        	//writer.write(current + "\n");
+			            current = arr1.get(i);
+			            writer.write(Long.toString(current) + "\n");
+			            found = false;
+			        }
+			    }
+				
+				
+				arr2 = null;
+				reader2.close();
+			
+			
+			arr1 = null;
+			
+		
+		
+		reader.close();
+		
+		
+		writer.close();
+		
+		
+	
 	}
-
-}
